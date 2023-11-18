9a71df0078780db00d178944bcd15a5b6f746c7f
Moulee109 <150134728+Moulee109@users.noreply.github.com>
2023-11-18 12:15:01 +0530

    Add files via upload

---
+++ Activity 5/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Unmount from './Components/Unmount';
+
+function App() {
+  return (
+    <div className="App">
+     <Unmount/>
+     <div>
+      </div>
+    </div>
+  );
+}
+export default App;
